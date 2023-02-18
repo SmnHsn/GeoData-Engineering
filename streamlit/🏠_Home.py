370ae9d56ff915feb18fff43eab62d1043a49387
simon hosny <simonhosny@laposte.net>
2023-02-18 19:12:47 +0100

    Initiating the Streamlit tameplate

---
+++ streamlit/🏠_Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋"
+)
+
+st.image('img/background.jpg')
+
+st.write(
+    """
+    ### **👈 The sidebar on the left provides access to a GeoData engineering project.**
+    > This web app was realised using the open-source library **Streamlit**.
+"""
+)
