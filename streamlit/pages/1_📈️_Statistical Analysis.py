0398de6a8d96af189916b5fafac4875439633744
simon hosny <simonhosny@laposte.net>
2023-03-01 17:10:35 +0100

    No modification

--- streamlit/pages/1_📈️_Statistical Analysis.py
+++ streamlit/pages/1_📈️_Statistical Analysis.py
@@ -1,1 +1,13 @@
-# Testing
+import streamlit as st
+
+st.sidebar.selectbox(
+    'Soil type to analyse',
+    ('Clay', 'Sand', 'Limestone'),
+                     )
+
+st.sidebar.selectbox(
+    'Geotechnical parameter to analyse',
+    ('Em', 'Pl*'),
+                     )
+
+st.sidebar.checkbox('Using the log of the parameter?')
